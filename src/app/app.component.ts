7e3af48a21f0f9b541d90164b9054d75d5a59385
Kerr Campbell - The Digital Solver <kerr.digitalsolver@gmail.com>
2022-12-19 18:00:13 +0000

    Removing the migrated html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -14,22 +11,4 @@ export class AppComponent {
   title = 'myFlix-Angular-client';
 
   constructor(public dialog: MatDialog) { }
-
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px'
-    });
-  }
-
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
-    });
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
 }
